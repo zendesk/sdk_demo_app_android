eaf8a285f87827528c8da8eccda30578281be7b4
Sebastian Chlan <schlan@zendesk.com>
2015-04-23 11:48:22 +0100

    Added custom ticketfields

--- app/src/main/java/com/zendesk/rememberthedate/ui/MainActivity.java
+++ app/src/main/java/com/zendesk/rememberthedate/ui/MainActivity.java
@@ -2,9 +2,14 @@ package com.zendesk.rememberthedate.ui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
+import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StatFs;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -16,19 +21,27 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.zendesk.rememberthedate.BuildConfig;
 import com.zendesk.rememberthedate.R;
 import com.zendesk.rememberthedate.push.GcmUtil;
 import com.zendesk.rememberthedate.storage.PushNotificationStorage;
 import com.zendesk.rememberthedate.storage.UserProfileStorage;
 import com.zendesk.sdk.feedback.impl.BaseZendeskFeedbackConfiguration;
 import com.zendesk.sdk.logger.Logger;
+import com.zendesk.sdk.model.CustomField;
+import com.zendesk.sdk.model.DeviceInfo;
+import com.zendesk.sdk.model.MemoryInformation;
 import com.zendesk.sdk.model.network.ErrorResponse;
 import com.zendesk.sdk.model.network.JwtIdentity;
 import com.zendesk.sdk.model.network.PushRegistrationResponse;
 import com.zendesk.sdk.network.impl.ZendeskCallback;
 import com.zendesk.sdk.network.impl.ZendeskConfig;
+import com.zendesk.sdk.power.PowerConfig;
+import com.zendesk.sdk.util.FileUtils;
 import com.zendesk.sdk.util.StringUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import retrofit.client.Response;
@@ -41,7 +54,15 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
     public static final String EXTRA_VIEWPAGER_POSITION = "extra_viewpager_pos";
     public static final int VIEWPAGER_POS_DATES = 0;
     public static final int VIEWPAGER_POS_HELP = 1;
-    
+
+    private static final long TICKET_FORM_ID = 62599l;
+    private static final long TICKET_FIELD_APP_VERSION = 24328555l;
+    private static final long TICKET_FIELD_OS_VERSION = 24273979l;
+    private static final long TICKET_FIELD_DEVICE_MODEL = 24273989l;
+    private static final long TICKET_FIELD_DEVICE_MEMORY = 24273999;
+    private static final long TICKET_FIELD_DEVICE_FREE_SPACE = 24274009l;
+    private static final long TICKET_FIELD_DEVICE_BATTERY_LEVEL = 24274019l;
+
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
@@ -128,10 +149,65 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
 
         String email = mStorage.getProfile().getEmail();
 
+
         if (StringUtils.hasLength(email)){
             Logger.i("Identity", "Setting identity");
             ZendeskConfig.INSTANCE.setIdentity(new JwtIdentity(email));
         }
+
+        ZendeskConfig.INSTANCE.setTicketFormId(TICKET_FORM_ID);
+        ZendeskConfig.INSTANCE.setCustomFields(getCustomFields());
+    }
+
+    private List<CustomField> getCustomFields(){
+        final DeviceInfo deviceInfo = new DeviceInfo(this);
+        final MemoryInformation memoryInformation = new MemoryInformation(this);
+
+        final String appVersion = String.format(
+                Locale.US,
+                "version_%s",
+                BuildConfig.VERSION_NAME
+        );
+
+        final String osVersion = String.format(
+                Locale.US,
+                "Android %s, Version %s",
+                deviceInfo.getVersionName(), deviceInfo.getVersionCode()
+        );
+
+        final String deviceModel = String.format(
+                Locale.US,
+                "%s, %s, %s",
+                deviceInfo.getModelName(), deviceInfo.getModelDeviceName(), deviceInfo.getModelManufacturer()
+        );
+
+        final StatFs stat = new StatFs(Environment.getDataDirectory().getPath());
+        final long bytesAvailable = (long)stat.getBlockSize() * (long)stat.getAvailableBlocks();
+        final String freeSpace = FileUtils.humanReadableFileSize(bytesAvailable);
+
+        final String batteryLevel = String.format(Locale.US, "%.1f %s", getBatteryLevel(), "%");
+
+        final List<CustomField> customFields = new ArrayList<>();
+        customFields.add(new CustomField(TICKET_FIELD_APP_VERSION, appVersion));
+        customFields.add(new CustomField(TICKET_FIELD_OS_VERSION, osVersion));
+        customFields.add(new CustomField(TICKET_FIELD_DEVICE_MODEL, deviceModel));
+        customFields.add(new CustomField(TICKET_FIELD_DEVICE_MEMORY, memoryInformation.formatMemoryUsage()));
+        customFields.add(new CustomField(TICKET_FIELD_DEVICE_FREE_SPACE, freeSpace));
+        customFields.add(new CustomField(TICKET_FIELD_DEVICE_BATTERY_LEVEL, batteryLevel));
+
+        return customFields;
+    }
+
+    public float getBatteryLevel() {
+        final Intent batteryIntent = registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        final int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        final int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        if(level == -1 || scale == -1) {
+            return 50.0f;
+        }
+
+        return ((float)level / (float)scale) * 100.0f;
     }
 
     void initialisePush(){
